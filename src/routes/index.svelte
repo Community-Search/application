27eb4c5cc4d52e9dd0005e7aa8a60e6b738d49e5
Victor Frunza <victor.frunza96@gmail.com>
2022-04-19 16:04:36 -0700

    converted the project to svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,304 @@
+<script>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/env';
+
+    onMount(async () => {
+        if(browser) {
+            const leaflet = await import('leaflet');
+
+            const map = leaflet.map('map').setView([53.54, -113.49], 12);
+
+            leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmZydW56YSIsImEiOiJjbDIyZGMxN2MwM2VlM2pydTZtMGkxaGN4In0.jATb6FcsSJj8ZZiWt9GQ4w', {
+                attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+            }).addTo(map);
+        }
+    });
+</script>
+
+
+<style>
+    @import 'https://unpkg.com/leaflet@1.8.0/dist/leaflet.css';
+</style>
+
+
+<div class="h-full flex">
+    <!-- Narrow sidebar -->
+    <div class="hidden w-28 bg-teal-700 overflow-y-auto md:block">
+      <div class="w-full py-6 flex flex-col items-center">
+        <div class="flex-shrink-0 flex items-center text-white">
+            <h1><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M9 9a2 2 0 114 0 2 2 0 01-4 0z" />
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a4 4 0 00-3.446 6.032l-2.261 2.26a1 1 0 101.414 1.415l2.261-2.261A4 4 0 1011 5z" clip-rule="evenodd" />
+            </svg></h1>
+        </div>
+        <div class="flex-1 mt-6 w-full px-2 space-y-1">
+          <!-- Current: "bg-teal-800 text-white", Default: "text-teal-100 hover:bg-teal-800 hover:text-white" -->
+          <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium">
+            <!--
+              Heroicon name: outline/home
+  
+              Current: "text-white", Default: "text-teal-300 group-hover:text-white"
+            -->
+            <svg class="text-teal-300 group-hover:text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg>
+            <span class="mt-2">Home</span>
+          </a>
+  
+          <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium">
+            <!-- Heroicon name: outline/view-grid -->
+            <svg class="text-teal-300 group-hover:text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+            </svg>
+            <span class="mt-2">Areas</span>
+          </a>
+  
+          <a href="#" class="bg-teal-800 text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium" aria-current="page">
+            <!-- Heroicon name: outline/photograph -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <span class="mt-2">Places</span>
+          </a>
+  
+          <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium">
+            <!-- Heroicon name: outline/user-group -->
+            <svg class="text-teal-300 group-hover:text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+            <span class="mt-2">Participants</span>
+          </a>
+  
+          <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium">
+            <!-- Heroicon name: outline/collection -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+            </svg>
+            <span class="mt-2">Messages</span>
+          </a>
+  
+          <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium">
+            <!-- Heroicon name: outline/cog -->
+            <svg class="text-teal-300 group-hover:text-white h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <span class="mt-2">Settings</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  
+    <!--
+      Mobile menu
+  
+      Off-canvas menu for mobile, show/hide based on off-canvas menu state.
+    -->
+    <div class="md:hidden" role="dialog" aria-modal="true">
+      <div class="fixed inset-0 z-40 flex">
+        <!--
+          Off-canvas menu overlay, show/hide based on off-canvas menu state.
+  
+          Entering: "transition-opacity ease-linear duration-300"
+            From: "opacity-0"
+            To: "opacity-100"
+          Leaving: "transition-opacity ease-linear duration-300"
+            From: "opacity-100"
+            To: "opacity-0"
+        -->
+        <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+  
+        <!--
+          Off-canvas menu, show/hide based on off-canvas menu state.
+  
+          Entering: "transition ease-in-out duration-300 transform"
+            From: "-translate-x-full"
+            To: "translate-x-0"
+          Leaving: "transition ease-in-out duration-300 transform"
+            From: "translate-x-0"
+            To: "-translate-x-full"
+        -->
+        <div class="relative max-w-xs w-full bg-teal-700 pt-5 pb-4 flex-1 flex flex-col">
+          <!--
+            Close button, show/hide based on off-canvas menu state.
+  
+            Entering: "ease-in-out duration-300"
+              From: "opacity-0"
+              To: "opacity-100"
+            Leaving: "ease-in-out duration-300"
+              From: "opacity-100"
+              To: "opacity-0"
+          -->
+          <div class="absolute top-1 right-0 -mr-14 p-1">
+            <button type="button" class="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white">
+              <!-- Heroicon name: outline/x -->
+              <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+              <span class="sr-only">Close sidebar</span>
+            </button>
+          </div>
+  
+          <div class="flex-shrink-0 px-4 flex items-center">
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white" alt="Workflow">
+          </div>
+          <div class="mt-5 flex-1 h-0 px-2 overflow-y-auto">
+            <nav class="h-full flex flex-col">
+              <div class="space-y-1">
+                <!-- Current: "bg-teal-800 text-white", Default: "text-teal-100 hover:bg-teal-800 hover:text-white" -->
+                <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium">
+                  <!--
+                    Heroicon name: outline/home
+  
+                    Current: "text-white", Default: "text-teal-300 group-hover:text-white"
+                  -->
+                  <svg class="text-teal-300 group-hover:text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                  </svg>
+                  <span>Home</span>
+                </a>
+  
+                <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium">
+                  <!-- Heroicon name: outline/view-grid -->
+                  <svg class="text-teal-300 group-hover:text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                  </svg>
+                  <span>All Files</span>
+                </a>
+  
+                <a href="#" class="bg-teal-800 text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium" aria-current="page">
+                  <!-- Heroicon name: outline/photograph -->
+                  <svg class="text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                  </svg>
+                  <span>Photos</span>
+                </a>
+  
+                <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium">
+                  <!-- Heroicon name: outline/user-group -->
+                  <svg class="text-teal-300 group-hover:text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                  <span>Shared</span>
+                </a>
+  
+                <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium">
+                  <!-- Heroicon name: outline/collection -->
+                  <svg class="text-teal-300 group-hover:text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                  </svg>
+                  <span>Albums</span>
+                </a>
+  
+                <a href="#" class="text-teal-100 hover:bg-teal-800 hover:text-white group py-2 px-3 rounded-md flex items-center text-sm font-medium">
+                  <!-- Heroicon name: outline/cog -->
+                  <svg class="text-teal-300 group-hover:text-white mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  </svg>
+                  <span>Settings</span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </div>
+  
+        <div class="flex-shrink-0 w-14" aria-hidden="true">
+          <!-- Dummy element to force sidebar to shrink to fit close icon -->
+        </div>
+      </div>
+    </div>
+  
+    <!-- Content area -->
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <header class="w-full">
+        <div class="relative z-10 flex-shrink-0 h-16 bg-white border-b border-gray-200 shadow-sm flex">
+          <button type="button" class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
+            <span class="sr-only">Open sidebar</span>
+            <!-- Heroicon name: outline/menu-alt-2 -->
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+          </button>
+          <div class="flex-1 flex justify-between px-4 sm:px-6">
+            <div class="flex-1 flex">
+                <nav class="flex" aria-label="Breadcrumb">
+                <ol role="list" class="flex items-center space-x-4">
+                    <li>
+                    <div class="flex items-center">
+                        <svg class="flex-shrink-0 h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                        <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                        </svg>
+                        <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Alberta</a>
+                    </div>
+                    </li>
+                
+                    <li>
+                    <div class="flex items-center">
+                        <svg class="flex-shrink-0 h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                        <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                        </svg>
+                        <a href="#" class="ml-4 mr-5 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Edmonton</a>
+                    </div>
+                    </li>
+                </ol>
+                </nav>
+                  
+            </div>
+            <div class="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
+              <!-- Profile dropdown -->
+              <div class="relative flex-shrink-0">
+                <div>
+                  <button type="button" class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                    <span class="sr-only">Open user menu</span>
+                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
+                  </button>
+                </div>
+  
+                <!--
+                  Dropdown menu, show/hide based on menu state.
+  
+                  Entering: "transition ease-out duration-100"
+                    From: "transform opacity-0 scale-95"
+                    To: "transform opacity-100 scale-100"
+                  Leaving: "transition ease-in duration-75"
+                    From: "transform opacity-100 scale-100"
+                    To: "transform opacity-0 scale-95"
+                -->
+
+              </div>
+  
+              <button type="button" class="flex bg-teal-600 p-1 rounded-full items-center justify-center text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <!-- Heroicon name: outline/plus-sm -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                </svg>
+                <span class="sr-only">Add file</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+  
+      <!-- Main content -->
+      <div class="flex-1 flex items-stretch overflow-hidden">
+        <main class="flex-1 overflow-y-auto">
+          <!-- Primary column -->
+          <section aria-labelledby="primary-heading" class="min-w-0 flex-1 h-full flex flex-col lg:order-last">
+            <h1 id="primary-heading" class="sr-only">Photos</h1>
+            <!-- Your content -->
+
+            <div id="map" class="w-full h-full"></div>
+          </section>
+        </main>
+  
+        <!-- Secondary column (hidden on smaller screens) -->
+        <aside class="hidden w-96 bg-white border-l border-gray-200 overflow-y-auto lg:block">
+          <!-- Your content -->
+
+
+        </aside>
+      </div>
+    </div>
+  </div>
